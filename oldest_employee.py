d62145581d7ce8210da994a859f675aae28297d5
e_akhmedzhanov <e.akhmedzhanov@gmail.com>
2023-03-02 04:58:39 +0300

    oldest_employee.py - the solution was founded

--- oldest_employee.py
+++ oldest_employee.py
@@ -1,22 +1,31 @@
 '''
-Дан список сотрудников организации, в котором указаны их фамилии, имена и даты рождения. Напишите программу, которая определяет самого старшего сотрудника из данного списка.
+Дан список сотрудников организации, в котором указаны их фамилии, 
+имена и даты рождения. Напишите программу, которая определяет 
+самого старшего сотрудника из данного списка.
 '''
 
-from datetime import datetime as dt, timedelta as tm
+from datetime import datetime as dt
 
 
-n = int(input())
+n = int(input('Введите количество сотрудников: ')) # n = 4 для данного теста
 emp_info = {}
 lst = [
     'Иван Петров 01.05.1995',
     'Петр Сергеев 29.04.1995',
-    'Сергей Иванов 01.01.1996'
+    'Сергей Иванов 01.01.1996',
+    'Сергей Ивановский 01.01.1996'
     ]
-
-
 
 for i in range(len(lst)):
     f_name, l_name, d = lst[i].split()
     emp_info[f'{f_name} {l_name}'] = dt.strptime(d, '%d.%m.%Y')
 
-print(emp_info)
+oldest_emp_birthday = min(emp_info.values())
+counter = 0
+for key, value in emp_info.items():
+    if (dt.today() - value).days == (dt.today() - oldest_emp_birthday).days:
+        counter += 1
+if counter > 1:
+    print(oldest_emp_birthday.date().strftime('%d.%m.%Y'), counter)
+else:
+    print(oldest_emp_birthday.date().strftime('%d.%m.%Y'), min(emp_info, key=emp_info.get))
