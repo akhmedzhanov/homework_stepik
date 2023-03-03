6794fc5e872a6c739cb03a15ee15eb4ffc706436
e_akhmedzhanov <e.akhmedzhanov@gmail.com>
2023-03-03 05:36:50 +0300

    error in 8 of 10 test bday_in_the_next_week.py

--- bday_in_the_next_week.py
+++ bday_in_the_next_week.py
@@ -5,9 +5,9 @@
 семи дней от указанной даты.
 '''
 
-from datetime import datetime as dt, timedelta as td
+from datetime import datetime as dt, timedelta as td, date
 
-now_date = dt.strptime('14.11.2021', '%d.%m.%Y')
+now_date = dt.strptime('14.11.2021', '%d.%m.%Y') + td(days=1)
 n = 3
 lst = [
     'Иван Петров 16.11.1995',
@@ -20,5 +20,16 @@ for i in range(len(lst)):
     f_name, l_name, d = lst[i].split()
     b_dates[dt.strptime(d, '%d.%m.%Y')] = f'{f_name} {l_name}'
 
-for d in range(int(now_date.timestamp()), int((now_date + td(days=7)).timestamp()), 86400):
-    print(dt.fromtimestamp(d).date())
+next_week_temp = []
+step = 86400 #sec*min*hours
+for d in range(int(now_date.timestamp()), int((now_date + td(days=7)).timestamp()), step):
+    next_week_temp.append(dt.fromtimestamp(d).date())
+
+flag = True
+for d in sorted(b_dates, reverse=True):
+    if date(now_date.year, d.month, d.day) in next_week_temp:
+        print(b_dates[d])
+        flag = False
+        break
+if flag:
+    print('Дни рождения не планируются')
